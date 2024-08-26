4b4e8dd10cd75c0a3fc259683d364ca35a1679a4
Rafael Framil <raframil@gmail.com>
2024-08-26 20:04:53 -0300

    initial setup

---
+++ components/Header.tsx
@@ -0,0 +1,57 @@
+'use client'
+import headerNavLinks from '@/data/headerNavLinks'
+import LogoDarkMode from '@/data/logo_d_mode.svg'
+import LogoWhiteMode from '@/data/logo_w_mode.svg'
+import siteMetadata from '@/data/siteMetadata'
+import { useTheme } from 'next-themes'
+import Link from './Link'
+import MobileNav from './MobileNav'
+import SearchButton from './SearchButton'
+import ThemeSwitch from './ThemeSwitch'
+
+const Header = () => {
+  const { theme } = useTheme()
+  let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
+  if (siteMetadata.stickyNav) {
+    headerClass += ' sticky top-0 z-50'
+  }
+
+  return (
+    <header className={headerClass}>
+      <Link href="/" aria-label={siteMetadata.headerTitle}>
+        <div className="flex items-center justify-between">
+          <div className="mr-3 h-[100px] w-[100px]">
+            {theme === 'light' ? <LogoWhiteMode /> : <LogoDarkMode />}
+          </div>
+          {typeof siteMetadata.headerTitle === 'string' ? (
+            <div className="hidden h-6 text-2xl font-semibold sm:block">
+              {siteMetadata.headerTitle}
+            </div>
+          ) : (
+            siteMetadata.headerTitle
+          )}
+        </div>
+      </Link>
+      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+        <div className="no-scrollbar hidden max-w-40 items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96">
+          {headerNavLinks
+            .filter((link) => link.href !== '/')
+            .map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+              >
+                {link.title}
+              </Link>
+            ))}
+        </div>
+        <SearchButton />
+        <ThemeSwitch />
+        <MobileNav />
+      </div>
+    </header>
+  )
+}
+
+export default Header
